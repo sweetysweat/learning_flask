839a21c7420eed421a5021863ff0839f64004ffe
sweetysweat <aursegov000@gmail.com>
2022-01-11 00:20:42 +0300

    шаблон index и сам код

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+menu = ["1", "2", "3"]
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html", menu=menu)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About", menu=menu)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
