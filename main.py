0c519c5a55e4800a4be8f9a15ff592c10e0181b9
sweetysweat <aursegov000@gmail.com>
2022-01-14 21:30:43 +0300

    добавлены новые страницы и форма для отправления запросов

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["1", "2", "3"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Перове приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 
 @app.route("/")
-@app.route("/index")
 def index():
     return render_template("index.html", menu=menu)
 
@@ -16,5 +17,18 @@ def about():
     return render_template("about.html", title="About", menu=menu)
 
 
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form["username"])
+    return render_template("contact.html", title="Обратная связь", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"User: {username}"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
