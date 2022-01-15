e3953a97f8fc151ca54a85fdd0e882d18dabef17
sweetysweat <aursegov000@gmail.com>
2022-01-15 21:24:35 +0300

    аторизация и перенаправление на страницы пока говнокод внутри который будет переписываться

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "itsasecret"
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Перове приложение", "url": "first-app"},
@@ -20,15 +22,34 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        print(request.form["username"])
+        if len(request.form["username"]) > 2:
+            flash("Сообщение отправлено", category="success")
+        else:
+            flash("Ошибка", category="error")
     return render_template("contact.html", title="Обратная связь", menu=menu)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page404.html", title="Страница не найдена", menu=menu), 404
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if "user_logged" in session:
+        return redirect(url_for("profile", username=session["user_logged"]))
+    elif request.method == "POST" and request.form["username"] == "111" and request.form["password"] == "111":
+        session["user_logged"] = request.form["username"]
+        return redirect(url_for("profile", username=session["user_logged"]))
+    return render_template("login.html", title="Авторизация", menu=menu)
 
 
 @app.route("/profile/<username>")
 def profile(username):
+    if "user_logged" not in session or session["user_logged"] != username:
+        abort(401)
     return f"User: {username}"
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
